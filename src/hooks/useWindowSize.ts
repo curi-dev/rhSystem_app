7f09e389cbc90185b981ae14b2202b6e6f16a6c1
curi <shopper.tiago@gmail.com>
2023-09-05 11:16:15 -0300

    use client

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,22 +1,33 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 
 function useWindowSize() {
 
-    const [size, setSize] = useState({
-        height: window.innerHeight,
-        width: window.innerWidth
+    const [size, setSize] = useState(() => {
+        if (typeof window !== undefined) {
+            return {
+                height: window.innerHeight,
+                width: window.innerWidth
+            }
+        } else {
+            return  {
+                height: 0,
+                width: 0
+            }
+        }
     })
 
     console.log("size: ", size)
 
     const updateWindowSize = () => {
-        setSize({
-            height: window.innerHeight,
-            width: window.innerWidth
-        })
+        if (typeof window !== undefined) {
+            setSize({
+                height: window.innerHeight,
+                width: window.innerWidth
+            })
+        }
     }
 
     const isLessThan = (value: number): boolean => {
@@ -28,9 +39,11 @@ function useWindowSize() {
     }
 
     useEffect(() => {
-        window.addEventListener('resize', updateWindowSize)
-
-        return () => window.removeEventListener('resize', updateWindowSize)
+        if (typeof window !== undefined) {
+            window.addEventListener('resize', updateWindowSize)
+    
+            return () => window.removeEventListener('resize', updateWindowSize)
+        }
     }, [])
 
 
