93d44783be10e10c798d0df229a87b2359e2a2a7
curi <shopper.tiago@gmail.com>
2023-09-06 13:57:08 -0300

    test

--- src/app/marcados/page.tsx
+++ src/app/marcados/page.tsx
@@ -55,7 +55,7 @@ const Scheduled = () => {
         if (appointments.length > 0) {
             if (!!acc.length) {
                 console.log("currValue: ", currValue)
-                if (currValue.datetime?.toISOString() !== (acc[acc.length -1][0]['Date'] as Date).toISOString()) {
+                if (currValue.datetime !== (acc[acc.length -1][0]['Date'] as Date)) {
                     console.log("currValue: II", currValue)
                     // @ts-ignore
                     acc.push([currValue])
